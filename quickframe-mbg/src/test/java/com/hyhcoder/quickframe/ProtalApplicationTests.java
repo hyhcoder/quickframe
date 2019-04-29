360ee27d6b045390470cf8e6cc35328884c64a0d
hyhcoder <hyuhan@126.com>
2019-04-29 08:05:19 +0800

    更新配置和简化了pom

--- quickframe-mbg/src/test/java/com/hyhcoder/quickframe/ProtalApplicationTests.java
+++ quickframe-mbg/src/test/java/com/hyhcoder/quickframe/ProtalApplicationTests.java
@@ -4,7 +4,6 @@ import com.hyhcoder.quickframe.mapper.PmsProductMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
--- quickframe-protal/src/main/java/com/hyhcoder/protal/ProtalApplication.java
+++ quickframe-protal/src/main/java/com/hyhcoder/protal/ProtalApplication.java
@@ -1,9 +1,11 @@
 package com.hyhcoder.protal;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hyhcoder.quickframe.mapper")
 public class ProtalApplication {
 
     public static void main(String[] args) {
